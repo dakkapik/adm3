899e96146f77059f7cbce80d91dbc0413af7378a
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2023-10-01 22:19:28 -0400

    update const text

--- server/emitter/main.js
+++ server/emitter/main.js
@@ -1,1 +1,13 @@
-const emitter = require("./emitter")
+// const fs = require('fs/promises');
+const client = require("./emitter")
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question(`What's your name?`, name => {
+    client.write(name)
+    // console.log(`Hi ${name}!`);
+    readline.close();
+});
